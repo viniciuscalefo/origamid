a92ee8a64379b44139ecbd09be50d6663c77a4c5
Vinicius Calefo Assarice <116103355+viniciuscalefo@users.noreply.github.com>
2023-11-14 10:37:04 -0300

    .
    .

--- 05/0509-menu-mobile copy/js/modules/outsideclick.js
+++ 05/0509-menu-mobile copy/js/modules/outsideclick.js
@@ -1,11 +1,11 @@
 export default function outsideClick(element, events, callback) {
-  const html = document.documentElement;//selecionando o elemento html   
+  const html = document.documentElement;
   const outside = 'data-outside';
 
   if(!element.hasAttribute(outside)) {
     events.forEach(userEvent => {
-      html.addEventListener(userEvent, handleOutsideClick);
-    })
+      setTimeout(() => html.addEventListener(userEvent, handleOutsideClick));
+    });
     element.setAttribute(outside, '');
   }
   function handleOutsideClick(event) {
